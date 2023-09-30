8ebe6b610ae92b721376e150c284b3c222e90028
codeng22 <codeng22@gmail.com>
2023-09-30 20:38:34 +0530

    Form validation on user component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* This is the code from booking module  */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
